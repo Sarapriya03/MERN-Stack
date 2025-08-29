12a82bcaaf42c57f87eb20dd8b411b0549ffb6de
Sarapriya03 <sarapriya2912@gmail.com>
2025-08-29 09:48:13 +0530

    Update todoList.js

--- backend/models/todoList.js
+++ backend/models/todoList.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 
 const TodoSchema = new mongoose.Schema({
-  task: { type: String, required: false },       
-  status: { type: String, required: false },     
-  title: { type: String, required: false },      // accept frontend "title"
-  description: { type: String, required: false },// accept frontend "description"
-  completed: { type: Boolean, default: false },  
-  deadline: { type: Date }                       
-});
+  title: { type: String, required: true },        // "First Task"
+  description: { type: String, required: true },  // "Develop a MERN stack app"
+  completed: { type: Boolean, default: false },   // false
+  deadline: { type: Date, required: true }        // 2025-08-30T12:00
+}, { timestamps: true });
 
 module.exports = mongoose.model("Todo", TodoSchema);
